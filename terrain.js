314247c4bc1627caa100277b388af972398761bf
pluk74 <pluk74@hotmail.com>
2016-04-14 23:38:06 +0000

    Added terrain.js

--- terrain.js
+++ terrain.js
@@ -15,20 +15,24 @@ Go thru all the map again, randomly burning and freezing some tiles. Then, re-ou
 
 */
 
-function Tile (x,y) {
-    
+function Tile(x, y) {
+
     this.x = x;
-    this.y = y; 
-    this.height = getRandomInt(1,3);
+    this.y = y;
+    this.height = getRandomInt(1, 3);
     this.type = randomType("type");
     this.condition = randomType("condition");
-    this.freeze = function () {
+    this.freeze = function() {
         this.condition = "frozen";
+        this.height += 1;
     }
-    this.burn = function () {
+    this.burn = function() {
         this.condition = "normal";
+        if (this.height > 0) {
+            this.height -= 1;
+        }
     }
-    
+
 }
 
 function getRandomInt(min, max) {
@@ -38,29 +42,36 @@ function getRandomInt(min, max) {
 //type: a random word between grass, rocks or water
 //condition: a random word between burning, normal or frozen
 function randomType(propertyName) {
-    
-    var type = ["grass","rocks","water"];
-    var condition = ["burning","normal","frozen"];
-    
+
+    var type = ["grass", "rocks", "water"];
+    var condition = ["burning", "normal", "frozen"];
+
     if (propertyName === "type") {
-        return type[getRandomInt(0,2)];
+        return type[getRandomInt(0, 2)];
     }
     else if (propertyName === "condition") {
-        return condition[getRandomInt(0,2)];
+        return condition[getRandomInt(0, 2)];
     }
-    
+
 }
 
+function displayGrid() {
+    var world = [];
 
-
-
-
-Tile.prototype = {
-    
+    for (var i = 0; i <= 20; i++) {
+        var row = [];
+        for (var j = 0; j <= 20; j++) {
+            var tile = new Tile(i, j);
+            row.push(tile);
+        }
+        world.push(row)
+    }
+    return world;
 }
-
 var x = randomType("condition");
-var thing = new Tile(2,3);
+var thing = new Tile(2, 3);
 console.log(thing);
-thing.freeze();
+thing.burn();
 console.log(thing);
+
+console.log(displayGrid());
