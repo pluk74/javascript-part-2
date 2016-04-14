6d815cb39af5a085ed1fdc84809b385d95de69e8
pluk74 <pluk74@hotmail.com>
2016-04-14 19:18:43 +0000

    Exercise 2: Array of Objects

--- functions.js
+++ functions.js
@@ -29,3 +29,53 @@ function compare (a,b) {
 
 //console.log(compare("asdfsadfds","wrewaresraeraersrf"));
 //console.log(retLongestString("chrysanthemum","alaj"));
+
+/*
+Exercise 2:
+Create an array of objects (don’t need to use new here, just regular object literals). 
+
+These objects will contain a name and email property. 
+Then, run some code that will sort your array by the longest name. 
+Then, run some code that will sort your array by e-mail address in alphabetical order.
+*/
+
+
+
+var people = [
+    {name : "Jerry", email : "sjerry@tv.com"},
+    {name : "Georgesdfsdf", email : "dgeorge@tv.com"},
+    {name : "Elainesdf", email : "xelaine@tv.com"},
+    {name : "Kramersdfsd", email : "tkramer@tv.com"}
+    ]
+
+function compareObjectName (a,b) {
+    
+    if (a.name.length >= b.name.length) {
+        return -1;
+    }
+    else {
+        return 1;
+    }
+}
+
+function compareObjectEmail (a,b) {
+    
+    if (a.email >= b.email) {
+        return 1;
+    }
+    else {
+        return -1;
+    }
+}
+
+
+people = people.sort(compareObjectEmail);
+
+//console.log(people);
+
+/*
+Create a function that can be used with Array.prototype.map. 
+
+This function should take a number and return its square. 
+Then, use this function with map on an array of numbers to check the result.
+*/
